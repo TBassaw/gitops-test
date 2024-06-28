a005e40ba4b81e41131efaf8b764e8a37236c0cc
TBassaw <abeikubassawt@gmail.com>
2024-06-28 15:37:01 +0000

    cronjob

--- versionStream/src/Makefile.mk
+++ versionStream/src/Makefile.mk
@@ -347,7 +347,7 @@ apply-ingressclass:
 preprocess-manifests:
 	find $(OUTPUT_DIR) -type f -name "*.yaml" -exec sed -i '/kind: KuberhealthyCheck/{:a;N;/podSpec:/!ba;s/podSpec:/podSpec:\n    restartPolicy: OnFailure\n    /}' {} +
 	find $(OUTPUT_DIR) -type f -name "*.yaml" -exec sed -i '/containers:/,/terminationGracePeriodSeconds:/{/restartPolicy: Never/d}' {} +
-	find $(OUTPUT_DIR) -type f -name "*.yaml" -exec sed -i '/kind: CronJob/,/containers:/ {s/containers:/  restartPolicy: OnFailure\n&/; s/^  restartPolicy: OnFailure/    restartPolicy: OnFailure/}' {} +
+	find $(OUTPUT_DIR) -type f -name "*.yaml" -exec sed -i '/kind: CronJob/,/containers:/ {s/containers:/restartPolicy: OnFailure\n          containers:/;}' {} +
 
 
 apply-other-resources:
