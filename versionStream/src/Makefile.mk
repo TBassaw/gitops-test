64168e2fe3dd4099aad51a5d32ff38768a2dfde6
TBassaw <abeikubassawt@gmail.com>
2024-07-01 01:46:52 +0000

    volume mounts

--- versionStream/src/Makefile.mk
+++ versionStream/src/Makefile.mk
@@ -352,8 +352,9 @@ preprocess-manifests:
 	find $(OUTPUT_DIR) -type f -name "*.yaml" -exec sed -i '/name: certmanager-tls/,/env:/ { s/env:/env: []/; }' {} +
 	find $(OUTPUT_DIR) -type f -name "kuberhealthy-pdb-poddisruptionbudget.yaml" -exec sed -i 's/apiVersion: policy\/v1beta1/apiVersion: policy\/v1/' {} +
 	find $(OUTPUT_DIR) -type f -name "kubernetes-external-secrets-deploy.yaml" -exec sed -i 's|"http://127.0.0.1:8200"|"https://vault.jx-vault:8200"|' {} +
-#	find $(OUTPUT_DIR) -type f -name "kubernetes-external-secrets-deploy.yaml" -exec sed -i '/kind: Deployment/,/kubernetes-external-secrets:/ { s/"http://127.0.0.1:8200"/"https://vault.jx-vault:8200"/; }' {} +
+	find $(OUTPUT_DIR) -type f -name "kubernetes-external-secrets-deploy.yaml" -exec sed -i '/securityContext:/i\        volumeMounts:\n        - name: vault-ca-cert\n          mountPath: /etc/ssl/certs\n          readOnly: true' {} +
 
+	
 
 apply-other-resources:
 	$(MAKE) apply-ingressclass
